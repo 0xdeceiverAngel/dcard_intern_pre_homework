c0060123182d3303c2d1b5649186d0c6dc6a608d
行者 <31663805+0xdeciverAngel@users.noreply.github.com>
2020-03-05 16:10:27 +0800

    mostly done

--- node.js
+++ node.js
@@ -1,6 +1,5 @@
 var mysql = require('mysql');
 var express = require('express');
-// const publicIp = require('public-ip');
 var router = express();
 
 var sql_connection = mysql.createConnection({
@@ -44,32 +43,24 @@ router.get('/alldata', function (req, res) {
 
 
     res.status(200);
-    // res.send('server on');
+    res.send('server on');
 });
 var ip;
+var current_datetime;
 router.get('/draw', function (req, res) {
     var sql;
     var req_times;
 
     ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
     sql = "SELECT req_times FROM ip_table  WHERE ip_addr=\"" + ip + "\"";
-    // sql = "SELECT req_times FROM ip_table  WHERE ip_addr=\"" +'123'+"\"";
-
-
-    // console.log(sql);
-    // console.log(ip);
-
-
     sql_connection.query(sql, function (err, result) {
         if (err) {
             console.log('[SELECT ERROR] - ', err.message);
             return;
         }
         if (result[0] == null) {
-            console.log('empty');
+            console.log('ip not in db');
             sql = "INSERT INTO `account`.`ip_table` (`ip_addr`, `req_times`, `dead_time`) VALUES (\"" + ip + "\",'1', NULL)";
-            // console.log(sql);
             req_times = 1;
             sql_connection.query(sql, function (err, result) {
                 if (err) {
@@ -87,25 +78,52 @@ router.get('/draw', function (req, res) {
             });
         }
         else {
-            console.log("not empty");
+            console.log("ip in db");
 
 
             req_times = result[0]['req_times'] + 1;
             if (req_times > 999) {
 
-                var current_datetime=new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-                console.log(current_datetime);
-                res.status(429);
-                res.set({
-                    'Error': '0',
-                    'X-RateLimit-Remaining': 1000 - req_times,
-                    'X-RateLimit-Reset': ''
+                current_datetime = new Date();
+                current_datetime.setHours(current_datetime.getHours() + 1 + 8);//+8 是因為時差 
+                current_datetime = current_datetime.toISOString().slice(0, 19).replace('T', ' ');
+
+                sql = "SELECT dead_time FROM ip_table  WHERE ip_addr=\"" + ip + "\"";
+
+                sql_connection.query(sql, function (err, result) {
+                    if (err) {
+                        console.log('[SELECT ERROR] - ', err.message);
+                        return;
+                    }
+                    if (result[0]['dead_time'] == null) {
+                        sql = "UPDATE `account`.`ip_table` SET `dead_time`='" + current_datetime + "' WHERE  `ip_addr`=\"" + ip + "\"";
+                        console.log("dead_time is null");
+                        sql_connection.query(sql, function (err, result) {
+                            if (err) {
+                                console.log('[SELECT ERROR] - ', err.message);
+                                return;
+                            }
+
+                        });
+                    }
+                    else {
+                        console.log("dead_time not null");
+                        current_datetime = result[0]['dead_time'];
+                    }
+                    res.status(429);
+                    res.set({
+                        'Error': '0',
+                        'X-RateLimit-Remaining': 0,
+                        'X-RateLimit-Reset': current_datetime
+                    });
+                    res.send('ip block');
                 });
-                res.send('ip block');
-                
+
+
+
+
             }
             else {
-                // console.log(req_times);
                 res.status(200);
                 res.set({
                     'Error': '0',
@@ -114,7 +132,6 @@ router.get('/draw', function (req, res) {
                 });
                 res.send(ip + "    draw" + req_times + "times");
                 sql = "UPDATE `account`.`ip_table` SET `req_times`='" + req_times + "' WHERE  `ip_addr`=\"" + ip + "\"";
-                console.log(sql);
                 req_times = 1;
                 sql_connection.query(sql, function (err, result) {
                     if (err) {
@@ -134,32 +151,9 @@ router.get('/draw', function (req, res) {
 
 
 });
-// INSERT INTO `account`.`ip_table` (`ip_addr`, `req_times`, `dead_time`) VALUES ('159.6.2.55', '20', '2020-03-04 20:44:01');
 
 router.listen(80, function () {
     console.log('Example app listening on port 80!');
 });
 
-
-
-// http.createServer(function (request, response) {
-
-//     // 发送 HTTP 头部 
-//     // HTTP 状态值: 200 : OK
-//     // 内容类型: text/plain
-//     response.writeHead(200, { 'Content-Type': 'text/plain' });
-//     response.writeHead(200, { 'Error': '1' });
-
-
-//     // 发送响应数据 "Hello World"
-//     response.end('Hello World\n');
-// }).listen(8888);
-
-
-
-
-// var sql = "SELECT * FROM file_path";
-
-
-// connection.end();
 
